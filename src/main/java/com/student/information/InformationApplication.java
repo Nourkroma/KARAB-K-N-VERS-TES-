fa174661f82b86a404908bcdce80b5669efb3ebc
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-03-04 18:59:02 +0200

    Exclude SecurityAutoConfiguration in InformationApplication.java

--- src/main/java/com/student/information/InformationApplication.java
+++ src/main/java/com/student/information/InformationApplication.java
@@ -2,10 +2,10 @@ package com.student.information;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@SpringBootApplication
-@CrossOrigin(origins = "*")
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class InformationApplication {
 
 	public static void main(String[] args) {
